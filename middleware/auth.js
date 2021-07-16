b4788405e6e4b754bac0a7ab32b65bbdcc838c21
GoodnessObi <goodnessobi@gmail.com>
2021-07-16 23:58:02 +0100

    auth middleware and protecting routes fix typo

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const congig = require('config');
+const config = require('config');
 
 module.exports = function(req, res, next) {
   //get token from header
@@ -12,6 +12,7 @@ module.exports = function(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
+
     req.user = decoded.user;
     next();
   } catch (err) {
