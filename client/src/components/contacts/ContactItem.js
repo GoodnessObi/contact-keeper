e33ee31c13811a01918ec8db9b2a1694dd8172aa
GoodnessObi <goodnessobi@gmail.com>
2021-07-23 00:00:09 +0100

    dispay contact list on home page

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -4,11 +4,8 @@ const ContactItem = ({ contact }) => {
   const { id, name, email, phone, type} = contact;
   return (
     <div className="card bg-light">
-        <h3 className="text-primary text-name">
-          {name}{' '}
-          <span className={'badge' + {type === 'professional' ? 'badge-success' : 'badge-primary'}}>
-            {type}
-          </span>
+        <h3 className="text-primary text-left">
+          {name}{' '}<span className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}>{type}</span>
         </h3>
     </div>
   )
