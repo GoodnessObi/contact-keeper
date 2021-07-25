dbd98a1d3a34ccaea2b49a2567d564d413cbfdd0
GoodnessObi <goodnessobi@gmail.com>
2021-07-25 21:09:17 +0100

    cleanup ui layout

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -35,8 +35,8 @@ const onSubmit = e => {
       <input type="text" placeholder='Phone' name='phone' value={phone} onChange={onChange} />
 
       <h5>Contact Type</h5>
-      <input type="radio" name='type' value="personal" checked={type === 'personal'} onChange={onChange} />Personal{' '}
-      <input type="radio" name='type' value="professional" checked={type === 'professional'} onChange={onChange} />Professional{' '}
+      <input type="radio" name='type' value="personal" checked={type === 'personal'} onChange={onChange} />{' '}Personal{' '}
+      <input type="radio" name='type' value="professional" checked={type === 'professional'} onChange={onChange} />{' '}Professional
 
       <input type="submit" value="Add Contact"  className="btn btn-primary btn-block" />
     </form>
