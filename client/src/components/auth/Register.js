41a544b6c7137a9f12e8db8ff872582471907ef1
GoodnessObi <goodnessobi@gmail.com>
2021-08-03 23:56:49 +0100

    user registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,21 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
   const { setAlert } = alertContext;
+  const { register, error, clearErrors } = authContext;
+
+  useEffect(() => {
+    if (error === 'user already exists') {
+      setAlert(error, 'danger')
+      clearErrors();
+    }
+  }, [error])
+
 
   const [user, setUser] = useState({
     name: '',
@@ -23,7 +35,11 @@ const Register = () => {
     } else if (password !== password2) {
       setAlert('Passwords do not match', 'danger')
     } else {
-      console.log('Registered')
+      register({
+        name,
+        email,
+        password
+      })
     }
   }
 
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -9,7 +10,8 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  CLEAR_ERRORS
+  CLEAR_ERRORS,
+  CONTACT_ERROR
 } from '../types';
 
 const AuthState = props => {
@@ -26,7 +28,26 @@ const AuthState = props => {
   //Load user 
 
   //Register User
+  const register = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }      
+    }
 
+    try {
+      const res = await axios.post('/api/users', formData, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      })
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      })
+    }
+  }
 
   //Login user
 
@@ -34,7 +55,7 @@ const AuthState = props => {
 
 
   //Clear Errors
-
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS })
 
   return (
     <AuthContext.Provider
@@ -43,7 +64,9 @@ const AuthState = props => {
       isAuthenticated: state.isAuthenticated,
       loading: state.loading,
       user: state.user,
-      error: state.error
+      error: state.error,
+      register,
+      clearErrors
     }}>
       { props.children }
     </AuthContext.Provider>
