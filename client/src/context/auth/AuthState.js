6e7486fb3c357dfd2a118a8c2046ad8fa23fcbf6
GoodnessObi <goodnessobi@gmail.com>
2021-08-05 23:33:11 +0100

    login user

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -68,10 +68,31 @@ const AuthState = props => {
     }
   }
 
-  //Load user
-
-
   //Login
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }      
+    }
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      })
+    }
+  }
+
+  //Logout
 
 
   //Clear Errors
@@ -86,6 +107,7 @@ const AuthState = props => {
       user: state.user,
       error: state.error,
       register,
+      login,
       loadUser,
       clearErrors
     }}>
